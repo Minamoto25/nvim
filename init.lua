f101ec41e225b25131b1f57db539aae05e782bba
Minamoto25 <2546432235@qq.com>
2025-03-21 21:01:10 +0800

    scroll left/right

--- init.lua
+++ init.lua
@@ -26,6 +26,8 @@ vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<Esc>', '<cmd>noh<CR>')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', 'L', 'zL')
+vim.keymap.set('n', 'H', 'zH')
 vim.keymap.set('v', 'K',":m '<-2<CR>gv=gv")
 vim.keymap.set('v', 'J',":m '>+1<CR>gv=gv")
 
